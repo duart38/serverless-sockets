d2eb0998b0adaa866917df1cbb7105492579b79e
duart38 <mr.codercuracao@gmail.com>
2021-04-02 17:34:53 +0200

    removed demo to kick all

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,5 @@ if (import.meta.main) {
   console.log(`websocket server is running on :${port}`);
   const socket = new Socket("./plugs");
   // ... other http code goes here ...
-  setTimeout(()=>{ // Demo kick everyone after 8 seconds
-    socket.kickAll();
-  }, 8000)
   for await (const req of serve(`:${port}`)) socket.accept(req);
 }
