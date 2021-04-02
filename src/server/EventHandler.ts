7a6a5a255e4ffbee66d3d4c6bcb518ebff48531f
duart38 <mr.codercuracao@gmail.com>
2021-04-02 17:37:41 +0200

    simplified

--- src/server/EventHandler.ts
+++ src/server/EventHandler.ts
@@ -3,7 +3,8 @@ import type { WebSocket } from "https://deno.land/std@0.90.0/ws/mod.ts";
 
 import Socket from "./Socket.ts";
 import { Watcher } from "../FS/FileWatcher.ts";
-import { PLUG_LENGTH } from "../interface/socketFunction.ts";
+import { PLUG_LENGTH, PlugFunction } from "../interface/socketFunction.ts";
+import NonBlocking from "./NonBlocking.ts";
 
 export async function HandleEvent(
   socket: Socket,
@@ -13,14 +14,13 @@ export async function HandleEvent(
   const fileWatcher: Watcher = socket.directoryWatcher;
   try {
     const m = await import(`../${fileWatcher.directory()}/${sanitizeEvent(message.event)}.ts?${fileWatcher.getHash()}`);
-    Object.keys(m).forEach(async (FName)=>{
-      try{
-        if (typeof m[FName] === "function" && validateFunctionShape(m[FName])) await m[FName](socket, message, from) 
-      }catch(e){
-        console.log(e);
-        from.send("error");
-      }
+
+    console.time("bruteForce"+message.event);
+    (Object.values(m) as PlugFunction[]).filter(v=>typeof v === "function" && validateFunctionShape(v))
+    .forEach((fn)=>{
+        NonBlocking.call(fn, socket, message, from);
     });
+    console.timeEnd("bruteForce"+message.event);
   } catch (error) {
     console.log(error);
     from.send("Invalid event");
@@ -41,7 +41,10 @@ function sanitizeEvent(eventString: string): string {
  * @HOT
  * @param x function.
  */
-function validateFunctionShape(x: ()=>void){
-  if(x.length != PLUG_LENGTH) throw new Error(`shape of ${x.name} is wrong. parameter count of ${x.length} needs to be ${PLUG_LENGTH}`);
+function validateFunctionShape(x: PlugFunction){
+  if(x.length != PLUG_LENGTH){
+    console.error(`shape of ${x.name} is wrong. parameter count of ${x.length} needs to be ${PLUG_LENGTH}`);
+    return false;
+  }
   return true
 }
