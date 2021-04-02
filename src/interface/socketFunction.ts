8e0b8ac5383f86006ae94f9f6635dd448eee1b15
duart38 <mr.codercuracao@gmail.com>
2021-04-02 13:43:54 +0200

    moved everythiong to src

--- src/interface/socketFunction.ts
+++ src/interface/socketFunction.ts
@@ -10,3 +10,8 @@ export type PlugFunction = (
   message: socketMessage,
   from: WebSocket,
 ) => void;
+
+/**
+ * The length of the plug function, this allows shape checking to fail early.
+ */
+export const PLUG_LENGTH = 3;
