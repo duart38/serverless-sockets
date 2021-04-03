f21299b29529e15e268363b74cecb906deb67098
duart38 <mr.codercuracao@gmail.com>
2021-04-03 13:23:42 +0200

    testing changes

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,5 @@
 import type { socketMessage } from "./interface/message.ts";
 
-
 function firstConnection(){
   const ws = new WebSocket("ws://localhost:8080");
   ws.addEventListener("message", (ev) => console.log(ev.data));
@@ -10,25 +9,4 @@ function firstConnection(){
   });
 }
 
-function secondConnection(){
-  const second = new WebSocket("ws://localhost:8080");
-  second.addEventListener("message", (ev) => console.log(ev.data));
-  second.addEventListener("open", () => { second.send(JSON.stringify({ event: "echo", payload: "Hello World" })) });
-}
-
-// firstConnection();
-secondConnection();
-
-/**
- * Brute force test below
- */
-// const CONNECTIONS = 2;
-// const testObject: socketMessage = { event: "decorate", payload: `Connection` };
-// const msg = JSON.stringify(testObject);
-// for(let i = 0; i < CONNECTIONS; i++){
-//   const ws = new WebSocket("ws://localhost:8080");
-//   ws.addEventListener("open", () => {
-//     ws.addEventListener("message",()=>console.log(`got message back from instance ${i}`))
-//     ws.send(msg);
-//   });
-// }
+firstConnection();
