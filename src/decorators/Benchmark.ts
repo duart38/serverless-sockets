ae6de97c19d436d9938a73d8b06bd603b5d01a96
duart38 <mr.codercuracao@gmail.com>
2021-04-03 17:18:16 +0200

    add jsdoc

--- src/decorators/Benchmark.ts
+++ src/decorators/Benchmark.ts
@@ -1,6 +1,12 @@
 import { PlugFunction } from "../interface/socketFunction.ts";
 
 export default class Measure {
+  /**
+   * Time the length it takes for a function to execute when triggered.
+   * @note Using this in production will result in a slow-down of ~1MS per timed call.
+   * @param func function to wrap
+   * @returns a new function that is wrapped with timing logic
+   */
   static $Timing(func: PlugFunction): PlugFunction {
     return (socket, message, from) =>{
         const timeLabel = `[TIME] - (E:${message.event})_(RID:${from.conn.rid})_${Math.round(Math.random() * 1000)}`;
