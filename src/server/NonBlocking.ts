5adab6fbc390b9e9bba2f9b2c9246c66a8524815
duart38 <mr.codercuracao@gmail.com>
2021-04-03 21:30:03 +0200

    add jsdoc

--- src/server/NonBlocking.ts
+++ src/server/NonBlocking.ts
@@ -1,4 +1,11 @@
 export default class NonBlocking{
+    /**
+     * Little nifty code to push the passed function off the call stack and eventually (immediatly) into the task queue.
+     * The task queue will wait until the call stack is empty before pushing back into the call stack.
+     * This can be used to deffer the function and execute other functions that are then pushed onto the stack.
+     * @param func 
+     * @param args 
+     */
     public static call<T>(func: any, ...args: unknown[]){
         setTimeout(function(...passed){passed[0](...passed.slice(1))},0, func, ...args)
     }
