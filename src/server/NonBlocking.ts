2d95394c2945ddf661d8a1ef3e714e6850457894
duart38 <mr.codercuracao@gmail.com>
2021-04-03 13:24:03 +0200

    made NonBlocking call complain less

--- src/server/NonBlocking.ts
+++ src/server/NonBlocking.ts
@@ -1,5 +1,5 @@
 export default class NonBlocking{
-    public static call(func: (...x:unknown[])=>unknown, ...args: unknown[]){
+    public static call<T>(func: any, ...args: unknown[]){
         setTimeout(function(...passed){passed[0](...passed.slice(1))},0, func, ...args)
     }
 }
