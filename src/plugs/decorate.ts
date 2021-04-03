b48bc4c7c4699d9f931c88ea773199745c1fd4e2
duart38 <mr.codercuracao@gmail.com>
2021-04-03 13:23:48 +0200

    testing slow task

--- src/plugs/decorate.ts
+++ src/plugs/decorate.ts
@@ -1,4 +1,22 @@
-import type { PlugFunction } from "../interface/socketFunction.ts";
+import Measure from "../decorators/Benchmark.ts";
 
-export const echo: PlugFunction = (socket, message, from) => {
-};
+function sleep(seconds: number) {
+    var start = new Date().getTime(),
+        delay = seconds * 1000;
+
+    while (true) {
+        if ((new Date().getTime() - start) > delay) {
+            break;
+        }
+    }
+}
+
+function slow_task() {
+    sleep(5);
+
+    return [4, 8, 15, 16, 23, 42];
+}
+export const decorated = Measure.$Timing((socket, message, from) => {
+    slow_task();
+    console.log("reached");
+})
