4032a278e0f5afd0b1f47c232ddbc3db0f8a6771
duart38 <mr.codercuracao@gmail.com>
2021-04-03 17:34:06 +0200

    add silent method to log

--- src/decorators/Log.ts
+++ src/decorators/Log.ts
@@ -1,7 +1,12 @@
 import singleton from "https://raw.githubusercontent.com/grevend/singleton/main/mod.ts";
 class Log {
-    static info(msg: string){}
-    static error(msg: string){}
+    public info(msg: string){}
+    public error(msg: string){}
+    public silent<I>(x: ()=>I): I | null {
+        try{return x()}catch(e){this.error(e)}
+        return null;
+    }
+
 }
 
 export const $Log  = singleton(()=>new Log());
