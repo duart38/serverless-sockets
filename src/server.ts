be4426ef01c53136db3c14f1a77732aefeff3707
duart38 <mr.codercuracao@gmail.com>
2021-05-02 10:56:52 +0200

    calling pre-load method for testing

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
+import { preLoadPlugs } from "./server/PreLoader.ts";
 import Socket from "./server/Socket.ts";
 
 // TODO: TLS? (depends on deployment style)
@@ -7,6 +8,7 @@ if (import.meta.main) {
   const port = Deno.args[0] || "8080"; // TODO: parse "port" flag for various server providers to supply
   console.log(`websocket server is running on :${port}`);
   const socket = new Socket("./plugs");
+  preLoadPlugs("./plugs");
   // ... other http code goes here ...
   for await (const req of serve(`:${port}`)) socket.accept(req);
 }
