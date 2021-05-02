9d142112bd2172c83ec12770b5f16fcae8956ca3
duart38 <mr.codercuracao@gmail.com>
2021-05-02 11:12:58 +0200

    added TLS option

--- src/config.js
+++ src/config.js
@@ -2,5 +2,32 @@ export const CONFIG = Object.freeze({
     /**
      * Limits the allowed payload size. skips further operations if the incoming size is greater.
      */
-    payloadLimit: 150
+    payloadLimit: 150,
+    /**
+     * If this is enabled the initial start of the server will load in all the socket functions.
+     * This will cache them resulting in the first request from a user being faster
+     */
+    preloadPlugs: false,
+
+    /** Indicates wether this server needs to load from TLS or not */
+    secure: false,
+
+    INSECURE: {
+        /** The port to listen on. */
+        port: Number(Deno.args[0]) || 8080,
+        /** A literal IP address or host name that can be resolved to an IP address.
+        * If not specified, defaults to `0.0.0.0`. */
+        hostname: "0.0.0.0",
+    },
+    TLS: {
+        /** The port to listen on. */
+        port: Number(Deno.args[0]) || 8080,
+        /** A literal IP address or host name that can be resolved to an IP address.
+        * If not specified, defaults to `0.0.0.0`. */
+        hostname: "0.0.0.0",
+        /** Server certificate file. */
+        certFile: "",
+        /** Server public key file. */
+        keyFile: "",
+    }
 })
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
+import { serveTLS, serve } from "https://deno.land/std@0.90.0/http/server.ts";
+import { CONFIG } from "./config.js";
 import { preLoadPlugs } from "./server/PreLoader.ts";
 import Socket from "./server/Socket.ts";
 
-// TODO: TLS? (depends on deployment style)
 // TODO: take plug function from CLI
 if (import.meta.main) {
-  const port = Deno.args[0] || "8080"; // TODO: parse "port" flag for various server providers to supply
-  console.log(`websocket server is running on :${port}`);
   const socket = new Socket("./plugs");
   preLoadPlugs("./plugs");
   // ... other http code goes here ...
-  for await (const req of serve(`:${port}`)) socket.accept(req);
+  if(CONFIG.secure){
+    console.log(`websocket server is running on :${CONFIG.TLS.port}`);
+    for await (const req of serveTLS(CONFIG.TLS)) socket.accept(req);
+  }else{
+    console.log(`websocket server is running on :${CONFIG.INSECURE.port}`);
+    for await (const req of serve(CONFIG.INSECURE)) socket.accept(req);
+  }
 }
