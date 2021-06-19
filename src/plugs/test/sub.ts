be644537332d5a71ade659e970d971533cfefccb
duart38 <mr.codercuracao@gmail.com>
2021-06-19 15:20:22 +0200

    adapted plug for new singleton and SF shape

--- src/plugs/test/sub.ts
+++ src/plugs/test/sub.ts
@@ -1,5 +1,7 @@
 import type { PlugFunction } from "../../interface/socketFunction.ts";
-export const echo: PlugFunction = (socket, message) => {
+import { socketS } from "../../server/Socket.ts";
+const socket = socketS.getInstance();
+export const echo: PlugFunction = (message, _from) => {
     console.log("I'm a sub")
     socket.broadcast(message);
 };
