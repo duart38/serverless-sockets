e53e334a9faee47b1aa90d3c74dffb235b250177
duart38 <mr.codercuracao@gmail.com>
2021-06-19 15:19:34 +0200

    adapted 404 plug for new socket function shape

--- src/plugs/404.ts
+++ src/plugs/404.ts
@@ -1,5 +1,5 @@
 import type { PlugFunction } from "../interface/socketFunction.ts";
-export const notFound: PlugFunction = (_socket, message) => {
+export const notFound: PlugFunction = (message, _from) => {
     // ... triggered when event was not found.
     console.log(message);
 }
