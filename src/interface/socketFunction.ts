c6776c6225ada28e9e1d9daece13454ab9c1c909
duart38 <mr.codercuracao@gmail.com>
2021-06-19 15:19:19 +0200

    changed socket function shape

--- src/interface/socketFunction.ts
+++ src/interface/socketFunction.ts
@@ -1,12 +1,11 @@
 import type { socketMessage } from "./message.ts";
-import Socket from "../server/Socket.ts";
 /**
  * Skeleton of the function that is called from EventHandler
  * @see HandleEvent
  */
 export type PlugFunction = (
-  socket: Readonly<Socket>,
-  message: socketMessage
+  message: socketMessage,
+  from: number
 ) => void;
 
 /**
