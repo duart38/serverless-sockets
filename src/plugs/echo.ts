86a6e0832268b3666a5be4558adbd30444c94cf8
duart38 <mr.codercuracao@gmail.com>
2021-06-19 15:20:08 +0200

    adpated plug for new socket singleton and new socket function shape

--- src/plugs/echo.ts
+++ src/plugs/echo.ts
@@ -1,10 +1,12 @@
 import type { PlugFunction } from "../interface/socketFunction.ts";
-export const echo: PlugFunction = (socket, message) => {
+import { socketS } from "../server/Socket.ts";
+const socket = socketS.getInstance();
+export const echo: PlugFunction = (message, _from) => {
   socket.broadcast(message);
 };
 
 // supports multiple functions in one plug
-export const test: PlugFunction = (_socket, message) => {
+export const test: PlugFunction = (message, _from) => {
   console.log("some other function")
   for(let i = 0; i < 20; i++){
     message.payload["name"] = "John" + i;
