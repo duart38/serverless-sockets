b732a5a760a0798a533c79a2b60d0fe0bb284bad
duart38 <mr.codercuracao@gmail.com>
2021-09-01 09:27:51 -0400

    made decorateAccessorsWP revokable

--- src/MISC/utils.ts
+++ src/MISC/utils.ts
@@ -1,5 +1,10 @@
 import { CONFIG } from "../config.js";
 
+export interface revokableProxy<T> {
+  value: T,
+  revoke: (()=>void)[]
+}
+
 export function decorateAccessors<T extends Record<string, unknown>>(obj: T, callBack: (val: unknown) => void) {
   if(CONFIG.nestedPayloadProxy){
     Object.entries(obj).forEach(([key, val]) => {
@@ -17,21 +22,36 @@ export function decorateAccessors<T extends Record<string, unknown>>(obj: T, cal
 });
 }
 
-export function decorateAccessorsWP<T extends Record<string, unknown>>(obj: T, callBack: (val: unknown, path: string[], obj: T) => void, path: string[] = []): T {
+/**
+ * Decorate accessors with path support (updates also give path information to form an instruction set)
+ * @param obj 
+ * @param callBack 
+ * @param path 
+ * @returns 
+ */
+export function decorateAccessorsWP<T extends Record<string, unknown>>(obj: T, callBack: (val: unknown, path: string[], obj: T) => void, path: string[] = []): revokableProxy<T> {
+  let revokableAcc = [];
 	if(CONFIG.nestedPayloadProxy){
 	  Object.entries(obj).forEach(([key, val]) => {
 		if (typeof val === "object") {
-		  (obj[key] as unknown) = decorateAccessorsWP(val as any, callBack, [...path, key]);
+      let sub = decorateAccessorsWP(val as any, callBack, [...path, key]);
+		  (obj[key] as unknown) = sub.value;
+      revokableAcc.push(sub.revoke);
 		}
 	  });
 	}
-	return new Proxy(obj, {
+  const {proxy, revoke} = Proxy.revocable(obj, {
 	  set: (obj, modifiedKey, value)=>{
 		Reflect.set(obj, modifiedKey, value);
 		callBack(value, [...path, modifiedKey as string], obj); // TODO: we could accelerate ignition and turbofan if we ensure the same data shape here (i.e. don't mix numbers with string etc)
 		return true;
 	  }
   });
+  revokableAcc.push(revoke);
+  return {
+    value: proxy,
+    revoke: revokableAcc
+  }
 }
 
 /**
