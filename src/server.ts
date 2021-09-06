40d687fd842078469d6757978df91cc77f17dc32
duart38 <mr.codercuracao@gmail.com>
2021-09-06 10:19:29 -0400

    ability to run from enclosed file introduced Log instead of console command

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,26 @@
 import { serveTLS, serve } from "https://deno.land/std@0.90.0/http/server.ts";
 import { CLI } from "./components/CLI.ts";
+import { Log, LogLevel } from "./components/Log.ts";
 import { CONFIG } from "./config.js";
 import { preLoadPlugs } from "./server/PreLoader.ts";
 import { socketS } from "./server/Socket.ts";
 
-const cli = CLI.instance();
+export function start(){
+  const cli = CLI.instance();
+  cli.onReady().then(async ()=>{
+      const socket = socketS.getInstance();
+      CONFIG.preloadPlugs && preLoadPlugs(CONFIG.plugsFolder);
+      // ... other http code goes here ...
+      if(CONFIG.secure){
+        Log.info({level: LogLevel.low, message:`websocket server is running on :${CONFIG.TLS.port}`});
+        for await (const req of serveTLS(CONFIG.TLS)) socket.accept(req);
+      }else{
+        Log.info({level: LogLevel.low, message:`websocket server is running on :${CONFIG.INSECURE.port}`});
+        for await (const req of serve(CONFIG.INSECURE)) socket.accept(req);
+      }
+  }).catch((e)=>{Log.error({level: LogLevel.medium, message: e})});
+}
 
-cli.onReady().then(async ()=>{
-  if (import.meta.main) {
-    const socket = socketS.getInstance();
-    CONFIG.preloadPlugs && preLoadPlugs(CONFIG.plugsFolder);
-    // ... other http code goes here ...
-    if(CONFIG.secure){
-      console.log(`websocket server is running on :${CONFIG.TLS.port}`);
-      for await (const req of serveTLS(CONFIG.TLS)) socket.accept(req);
-    }else{
-      console.log(`websocket server is running on :${CONFIG.INSECURE.port}`);
-      for await (const req of serve(CONFIG.INSECURE)) socket.accept(req);
-    }
-  }
-}).catch(()=>{})
-
+if(import.meta.main){
+  start();
+}
