6c4ce7aae9e61a3af3c04313d32b9dbb4e6cd457
duart38 <mr.codercuracao@gmail.com>
2021-09-23 15:28:31 -0400

    improved performance by moving to a full for loop

--- src/server/EventHandler.ts
+++ src/server/EventHandler.ts
@@ -22,8 +22,10 @@ export async function HandleEvent(
     if(fileWatcher.containsFile(sanitized)){
       const m = await import(`${fileWatcher.directory()}/${sanitized}.ts?${fileWatcher.getFileHash(sanitized)}`);
 
-      (Object.values(m) as AsyncGeneratorFunction[]).filter(v=>typeof v === "function" && validateFunctionShape(v))
-      .forEach(async (fn)=>{for await(const v of fn(message, from)) Socket.sendMessage(from, v as socketMessage);}); // TODO: forEach loops are slow for no reason.
+      const gFn = (Object.values(m) as AsyncGeneratorFunction[]).filter(v=>typeof v === "function" && validateFunctionShape(v))
+      for(let i = 0; i < gFn.length; i++) {
+        for await(const v of gFn[i](message, from)) Socket.sendMessage(from, v as socketMessage); // TODO: forEach loops are slow for no reason.
+      }
       // i don't trust weakRefs for message because of possible long running methods, so this will do
       (message as unknown) = null;
     }else{
