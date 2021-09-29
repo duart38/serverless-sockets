9f698dcfa1c48c84f54eb1ab83ac2aa8a07ec885
duart38 <mr.codercuracao@gmail.com>
2021-09-29 19:40:37 -0400

    small changes

--- src/plugs/echo.ts
+++ src/plugs/echo.ts
@@ -1,8 +1,8 @@
-import { socketMessage } from "../interface/message.ts";
+import { SocketMessage } from "../interface/message.ts";
 import { ModuleGenerator } from "../interface/socketFunction.ts";
 import Socket from "../server/Socket.ts";
-export async function* broadcast(message: socketMessage, _from: number): ModuleGenerator{
-  Socket.broadcast(message);
+export async function* broadcast(message: SocketMessage, from: number): ModuleGenerator{
+  Socket.broadcast(message, from);
   yield {
     event: "",
     payload: {}
