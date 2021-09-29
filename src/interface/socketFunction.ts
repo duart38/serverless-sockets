3fa16766a320c16abb1d53defe8586c75e42fe65
duart38 <mr.codercuracao@gmail.com>
2021-09-29 12:15:25 -0400

    changed ModuleGenerator to use yieldedSocketMessage

--- src/interface/socketFunction.ts
+++ src/interface/socketFunction.ts
@@ -1,6 +1,6 @@
-import type { socketMessage } from "./message.ts";
+import type { yieldedSocketMessage } from "./message.ts";
 
-export type ModuleGenerator =  AsyncGenerator<socketMessage, socketMessage | void, socketMessage | void>
+export type ModuleGenerator =  AsyncGenerator<yieldedSocketMessage, yieldedSocketMessage | void, yieldedSocketMessage | void>
 
 /**
  * The length of the plug function, this allows shape checking to fail early.
