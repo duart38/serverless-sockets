4e17f703276476e1890490c10f76fcc9501901cc
duart38 <mr.codercuracao@gmail.com>
2021-10-04 12:28:21 -0400

    removed unused code

--- src/plugs/multiyield.ts
+++ src/plugs/multiyield.ts
@@ -2,11 +2,9 @@ import { Log, LogLevel } from "../components/Log.ts";
 import { SocketMessage } from "../interface/message.ts";
 import { ModuleGenerator } from "../interface/socketFunction.ts";
 
-// supports multiple functions in one plug
 export async function* test(message: SocketMessage, _from: number): ModuleGenerator{
     Log.info({level: LogLevel.low, message: "some other test function"})
     for(let i = 0; i<message.payload.count; i++){
-      //message.payload["name"] = "John" + i; // <-- only works in proxy-mode
       yield {
         event: "spam-mode",
         payload: {
