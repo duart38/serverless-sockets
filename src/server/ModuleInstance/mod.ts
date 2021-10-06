17f0d21fd399fd7788c9f8feab8f9aced9d4ff1f
duart38 <mr.codercuracao@gmail.com>
2021-10-06 08:50:00 -0400

    removed unused folder

--- src/server/ModuleInstance/mod.ts
+++
@@ -1,8 +0,0 @@
-// main entry point for upcoming scalability stuff
-
-// auto launch a listener
-// listen for events
-// decode and run through event listener.
-
-// ---
-// event listener responds back on each yield with instructions on who to send it to.
