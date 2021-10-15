657c17c6b7920959a6b4f676e8aec7516b2a4830
duart38 <mr.codercuracao@gmail.com>
2021-10-15 09:06:08 -0400

    Documentation

--- src/components/Log.ts
+++ src/components/Log.ts
@@ -1,23 +1,42 @@
 import singleton from "https://raw.githubusercontent.com/grevend/singleton/main/mod.ts";
 
+/**
+ * The level of logging. the higher the level the more important the log is.
+ */
 export enum LogLevel {
     hidden, low, medium, high, extreme
 }
+/**
+ * The shape used to log messages using the custom Log class.
+ */
 interface LogShape {
     level: LogLevel,
     message: string
 }
 export class Log {
     private logThread: Worker;
+
+    /**
+     * The constructor for the Log class. Use the static methods to log messages.
+     */
     constructor(){
         this.logThread = new Worker(new URL("../MISC/Threads/LoggingThread.js", import.meta.url).href, { type: "module" });
     }
     public static info(t: LogShape){ $Log.getInstance().logThread.postMessage(t) }
     public static error(t: LogShape){ $Log.getInstance().logThread.postMessage(t) }
+
+    /**
+     * Runs te provided method while catching for any errors, if an error is found it is logged silently (not displayed) to the logger.
+     * @returns returns any value returned by the method, null otherwise.
+     */
     public static silent<I>(x: ()=>I): I | null {
         try{return x()}catch(e){Log.error({level: LogLevel.hidden, message: e})}
         return null;
     }
+
+    /**
+     * @returns all the messages stored in the Logs storage. flushes the storage.
+     */
     public getAllLogs(){
         return new Promise<LogShape[]>((res,_rej)=>{
             this.logThread.postMessage("flush")
@@ -26,6 +45,9 @@ export class Log {
         });
     }
 
+    /**
+     * Says it all....
+     */
     public clearLogs(){
         this.logThread.postMessage("clear")
     }
