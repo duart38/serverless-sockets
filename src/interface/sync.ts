dc3effd69da9461a1b808843ea756ac28fe19b6e
duart38 <mr.codercuracao@gmail.com>
2021-10-15 09:24:10 -0400

    doc

--- src/interface/sync.ts
+++ src/interface/sync.ts
@@ -1,7 +1,12 @@
+/**
+ * Instructions that are used when the user is transferring data via sync.
+ */
 export enum syncInstruction {
     modify, delete
 }
-
+/**
+ * The shape of an instruction to modify or delete a portion of the destination data when the user is transferring data via sync.
+ */
 export interface modificationInstruction {
     path: string[],
     value: unknown,
