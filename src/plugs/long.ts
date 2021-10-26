d85bb83e5fa036d8983fe3462ec9592f051e3800
duart38 <mr.codercuracao@gmail.com>
2021-10-26 11:03:00 -0400

    fixed ts error

--- src/plugs/long.ts
+++ src/plugs/long.ts
@@ -2,7 +2,7 @@ import { socketMessage } from "../interface/message.ts";
 import { ModuleGenerator } from "../interface/socketFunction.ts";
 export async function* long(message: socketMessage): ModuleGenerator {
     console.log("reached");
-    await new Promise(resolve => setTimeout(resolve, message.payload.ms as number));
+    await new Promise(resolve => setTimeout(resolve, (message.payload as Record<string, unknown>).ms as number));
     yield {
         event: "waiting",
         payload: {}
