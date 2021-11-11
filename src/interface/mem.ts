c01c0a4fcb6c5ff2ce50798cec5f9d23d915547a
duart38 <mr.codercuracao@gmail.com>
2021-11-11 14:21:58 -0400

    changed to  interface

--- src/interface/mem.ts
+++ src/interface/mem.ts
@@ -1,3 +1,7 @@
-export abstract class FreeAble {
-    public abstract free(): void;
+/**
+ * FreeAble classes are classes that contain code that can be run to do an early cleanup.
+ * I.E. will accelerate the GC process of the V8 Engine
+ */
+export interface FreeAble {
+    free(): void;
 }
