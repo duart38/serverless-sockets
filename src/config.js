147c483f14bcea8489808a16c16121b958abe908
duart38 <mr.codercuracao@gmail.com>
2021-11-18 09:50:01 -0400

    removed all config options for proxy

--- src/config.js
+++ src/config.js
@@ -60,33 +60,6 @@ export class configuration {
             /** Server public key file. */
             keyFile: "/Users/duartasnel/Local/WORK/BusinessOne/socket_demos/.cert/key.pem",
         };
-        /**
-         * Indicates wether incoming payloads should be recursively proxied. if true
-         * Proxying the payload will call itself to proxy any nested objects withing the incoming object.
-         * Turning this on will have a small perfomance impact on deep nested objects to about O(log2n)
-         */
-        nestedPayloadProxy = true;
-    
-        /**
-         * Modify the proxies of the incoming object to sync an object in the client-server..
-         * This has an initial perfomance impact, i.e. if you send allot of requests to the server this can slow things down a bit.
-         * With the above out of the way, if you send one or few requests and expect allot of data back in the client this option
-         * can significantly help.
-         * @see https://v8.dev/blog/optimizing-proxies
-         */
-        proxySyncIncomingData = false;
-        /**
-         * Only applies if proxySyncIncomingData is 'true'
-         */
-        proxySyncSettings = {
-            /**
-             * Indicates if we send back instructions on how to update the object instead of sending a duplicate.
-             * This can be used when the objects being synced are large, instead of replicating the entire object through the network
-             * this option responds with indexes and where to place what item.. Think of this as server side computing where in an object the update(s)
-             * need to occur and responding with the instructions to do so.
-             */
-            instructionReply: true
-        };
     
         /**
          * Indicates wether we should validate the modular functions shape. this checks if the parameter count is according to the framework specifications.
