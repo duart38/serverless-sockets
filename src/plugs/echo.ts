0cc4bfbc00e99c3c563359bcbccbb7c36a75cad6
duart38 <mr.codercuracao@gmail.com>
2021-11-22 11:24:38 -0400

    changed echo example to new broadcasting system

--- src/plugs/echo.ts
+++ src/plugs/echo.ts
@@ -1,11 +1,11 @@
+import { EventType } from "../interface/message.ts";
 import { SocketMessage } from "../interface/message.ts";
 import { ModuleGenerator } from "../interface/socketFunction.ts";
-import Socket from "../server/Socket.ts";
-export async function* broadcast(message: SocketMessage, from: number): ModuleGenerator{
-  Socket.broadcast(message, from);
+export async function* broadcast(message: SocketMessage, _from: number): ModuleGenerator {
   yield {
-    event: "",
-    payload: {}
+    type: EventType.BROADCAST,
+    event: "broadcast-1",
+    payload: message.payload
   }
 }
 
