43951cdea2c5098b3fecbc33ad20b298f5cb7395
duart38 <duartsnel@icloud.com>
2021-12-03 09:09:44 -0400

    added a file that is executed at startup

---
+++ src/INIT.ts
@@ -0,0 +1,9 @@
+/**
+ * This function is executed right after the socket server is created.
+ * Put logic here that is to be executed regardless of any event that is to be executed.
+ * 
+ * EXAMPLE: listen for incoming connection from the socket and send each of them a message.
+*/
+export function INIT(){
+    console.log("initial function called");
+}
