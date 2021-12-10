4b548d486f03a411dc51e54fb80de1de3c779300
duart38 <duartsnel@icloud.com>
2021-12-10 09:01:09 -0400

    [BREAKING] Revamped the Log API

--- src/components/Log.ts
+++ src/components/Log.ts
@@ -5,21 +5,15 @@ import { CONFIG } from "../config.js";
  * The level of logging. the higher the level the more important the log is.
  */
 export enum LogLevel {
-  hidden,
-  low,
-  medium,
-  high,
-  extreme,
-}
-export enum LogType {
   info,
+  warning,
   error,
 }
+
 /**
  * The shape used to log messages using the custom Log class.
  */
 interface LogShape {
-  type?: LogType;
   timeStamp?: string | number;
   level: LogLevel;
   message: string;
@@ -31,8 +25,15 @@ export class Log {
    * The constructor for the Log class. Use the static methods to log messages.
    */
   constructor() {}
+  private logFunction(level: LogLevel){
+    switch(level){
+      case LogLevel.error: return console.error;
+      case LogLevel.info: return console.info;
+      case LogLevel.warning: return console.warn;
+    }
+  }
   private printLog(data: LogShape) {
-    const logF = data.type === LogType.error ? console.error : console.log;
+    const logF = this.logFunction(data.level);
     logF(`[${new Date(data.timeStamp || Date.now())}] - ${data.level} - ${data.message}`);
   }
   private pushLog(data: LogShape, print = false) {
@@ -43,25 +44,20 @@ export class Log {
   private log(data: LogShape) {
     return new Promise<void>((res) => {
       setTimeout(() => {
-        if (CONFIG.logLevel >= data.level) {
-          switch (data.level) {
-            case LogLevel.hidden:
-              this.pushLog(data, false);
-              break;
-            default:
-              this.pushLog(data, CONFIG.printLogToConsole);
-          }
-        }
+        if (CONFIG.logLevel >= data.level) this.pushLog(data, CONFIG.printLogToConsole);
         res();
       }, 0);
     });
   }
 
-  public static info(t: LogShape) {
-    return $Log.getInstance().log({ type: LogType.info, timeStamp: Date.now(), ...t });
+  public static info(message: string) {
+    return $Log.getInstance().log({ level: LogLevel.info, message, timeStamp: Date.now()});
   }
-  public static error(t: LogShape) {
-    return $Log.getInstance().log({ type: LogType.error, timeStamp: Date.now(), ...t });
+  public static warning(message: string) {
+    return $Log.getInstance().log({ level: LogLevel.warning, message, timeStamp: Date.now()});
+  }
+  public static error(message: string) {
+    return $Log.getInstance().log({ level: LogLevel.error, message, timeStamp: Date.now()});
   }
 
   /**
@@ -72,7 +68,7 @@ export class Log {
     try {
       return x();
     } catch (e) {
-      Log.error({ level: LogLevel.hidden, message: e });
+      Log.error(e);
     }
     return null;
   }
