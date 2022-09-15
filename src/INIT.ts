100ac90846b9b851709d5321d9e2884167e8f72a
duart38 <duartsnel@icloud.com>
2022-09-15 17:51:41 +0200

    started passing socket class in INIT function
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/INIT.ts
+++ src/INIT.ts
@@ -1,9 +1,11 @@
+import Socket from "./server/Socket.ts";
+
 /**
  * This function is executed right after the socket server is created.
  * Put logic here that is to be executed regardless of any event that is to be executed.
  *
  * EXAMPLE: listen for incoming connection from the socket and send each of them a message.
  */
-export function INIT() {
+export function INIT(socket: Socket) {
   console.log("initial function called");
 }
