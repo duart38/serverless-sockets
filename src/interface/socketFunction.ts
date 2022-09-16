79592cc90645c430144b767349bb6e55a04d281e
duart38 <duartsnel@icloud.com>
2022-09-16 08:59:36 +0200

    enforcing type safety on ModuleGenerator return type
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- src/interface/socketFunction.ts
+++ src/interface/socketFunction.ts
@@ -1,9 +1,9 @@
-import type { yieldedSocketMessage } from "./message.ts";
+import type { serializable, yieldedSocketMessage } from "./message.ts";
 
 /**
  * The async generator method used for socket functions (modules).
  */
-export type ModuleGenerator<T = Record<string, unknown>> = AsyncGenerator<yieldedSocketMessage<T>, yieldedSocketMessage<T> | void, yieldedSocketMessage<T> | void>;
+export type ModuleGenerator<T extends serializable = Record<string, unknown>> = AsyncGenerator<yieldedSocketMessage<T>, yieldedSocketMessage<T> | void, yieldedSocketMessage<T> | void>;
 
 /**
  * The length of the plug function, this allows shape checking to fail early.
