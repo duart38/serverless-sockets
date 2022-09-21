9ebd8aaeddbae84c03c9a4b7540b9d097ad22bee
duart38 <duartsnel@icloud.com>
2022-09-21 12:56:26 +0200

    Added GPL3 as license
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- tests/serverless.test.ts
+++ tests/serverless.test.ts
@@ -1,3 +1,20 @@
+/*
+ *   Copyright (c) 2022 Duart Snel
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { assert, assertEquals, assertNotEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
 import { serve } from "https://deno.land/std@0.90.0/http/server.ts";
 import { CONFIG } from "../src/config.js";
